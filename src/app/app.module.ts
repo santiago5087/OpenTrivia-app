44c8a8c7d5a0f4edda84ea790c21418c99e953d7
Santiago Marín <esmarina@unal.edu.co>
2020-07-28 18:09:24 -0500

    Angular Material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { TriviaOptionsComponent } from './components/trivia-options/trivia-options.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    TriviaOptionsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/trivia-options/trivia-options.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trivia-options',
+  templateUrl: './trivia-options.component.html',
+  styleUrls: ['./trivia-options.component.scss']
+})
+export class TriviaOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
